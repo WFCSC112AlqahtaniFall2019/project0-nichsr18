f3c18bac493bb77b229675e864e62f1e08ec3cc0
nichsr18 <nichsr18@wfu.edu>
2019-08-29 16:32:48 -0400

    added "write some code" assignment

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+
+    int x;
+    int y;
+    int in1;
+    int in2;
+    int in3;
+    string temp;
+    int in4;
+    int in5;
+    int in6;
+    int count = 0;
+    cin >> in1;
+    cin >> in2;
+    cin >> in3;
+    getline(cin, temp);
+    cin >> in4;
+    cin >> in5;
+    cin >> in6;
+
+    for (x = -10; x < 11; x++) {
+        for (y = -10; y < 11; y++) {
+            if (((in1 * x) + (in2 * y) == in3) && ((in4 * x) + (in5 * y) == in6)) {
+                cout << x << " ";
+                cout << y;
+                count = 1;
+            }
+        }
+    }
+    if (count != 1) {
+        cout << "No Solutions";
+    }
+
+
+
     return 0;
 }
